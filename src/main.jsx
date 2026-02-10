5f0781da796a4fb84ee79ec8971c4f44bbd2ef9f
Amina Saleem <amina_0307@hotmail.com>
2026-02-10 14:51:47 +0000

    edit info.plist

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./style.css";
+import { Capacitor } from "@capacitor/core";
+import { StatusBar } from "@capacitor/status-bar";
+
+if (Capacitor.getPlatform() === "ios") {
+  async () => {
+    try {
+      await StatusBar.setOverlaysWebView({ overlay: false });
+    } catch (e) {
+      console.warn("StatusBar plugin not ready:", e);
+    }
+  };
+}
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
